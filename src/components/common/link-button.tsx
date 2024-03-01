39ae6b2472528b9d601e827f9be2ced2a3ba2653
MHP24 <mig.henriquezp@gmail.com>
2024-03-01 14:37:15 -0300

    refactor: hero section

--- src/components/common/link-button.tsx
+++ src/components/common/link-button.tsx
@@ -1,17 +1,21 @@
+import { cn } from '@/utils'
 import Link from 'next/link'
-import { type FC, type PropsWithChildren } from 'react'
+import { type HTMLProps, type FC, type PropsWithChildren, type Ref } from 'react'
 
-type Props = {
+type Props = HTMLProps<HTMLAnchorElement> & {
   href: string
+  ref?: Ref<HTMLAnchorElement> | undefined
 }
+export const LinkButton: FC<PropsWithChildren<Props>> = ({ className, children, ...rest }) => {
+  const base = 'flex gap-2 items-center justify-center min-w-full sm:min-w-fit border-2 border-c3 hover:bg-c3-1 py-2 rounded-full'
+  const responsive = 'px-2 sm:px-6'
+  const transition = 'transition-all duration-200'
 
-export const LinkButton: FC<PropsWithChildren<Props>> = ({ href, children }) => {
   return (
     <Link
-      href={href}
+      {...rest}
       target='_blank'
-      className='flex gap-3 items-center justify-center min-w-full sm:min-w-fit
-        border-2 border-c3 hover:bg-c3-1 py-2 rounded-full px-2 sm:px-6 transition-all duration-200'
+      className={cn(base, responsive, transition, className)}
     >
       {children}
     </Link>
--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -1,27 +1,58 @@
-import { secondaryFont } from '@/fonts'
+import Image from 'next/image'
+import { LinkButton } from '..'
 
 export const HeroSection = () => {
   return (
     <section
       id='inicio'
       className='h-screen w-full bg-fixed bg-cover bg-no-repeat bg-center
-      px-4'
+      px-4 grid place-items-center'
     >
 
-      <div className='h-full flex flex-col items-center text-center md:text-start md:items-start w-full px-20 justify-center z-1'
-      >
-        <h1 className='text-5xl md:text-7xl lg:text-8xl font-bold fade'>{'Miguel Henríquez Pacheco'}</h1>
+      <div className='grid grid-cols-2 w-[90%] z-10'>
+        <article className='flex flex-col gap-6'>
+          <h1 className='text-5xl font-semibold'>
+            ¡Hola!👋 Soy Miguel Henr&iacute;quez Pacheco
+          </h1>
 
-        <h2 className={`${secondaryFont.className} 
-          text-3xl md:text-5xl mt-10 
-          bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent fade`}
-        >
-          Desarrollador full stack
-        </h2>
+          <h3 className='text-2xl mt-2'>
+            Experto en procesos del &aacute;rea TI,
+            <span className='font-semibold'> desarrollador </span>
+            con <span className='font-semibold'>+1 año de experiencia, </span>
+            fan&aacute;tico del Cloud y DevOps.
+          </h3>
 
-        <div className='bg-c3-1 w-[40%] h-full absolute top-0 right-0 border-l-2 border-c3-1 rightToLeft'></div>
+          <ul className='flex items-center gap-6 mt-6'>
+            <li className='w-48'>
+              <LinkButton
+                href='https://github.com/MHP24'
+                className='bg-c8  border-c3-1 font-semibold'
+              >
+                <Image src={'/svgs/medias/github.svg'} width={30} height={30} alt='github'/>
+                Proyectos
+              </LinkButton>
+            </li>
+            <li className='w-48'>
+              <LinkButton
+                href=''
+                className='border-c3-1 bg-c3 text-c4 bg-opacity-90
+                  font-semibold hover:bg-c3 hover:bg-opacity-100 hover:text-c4'
+              >
+                <Image src={'/svgs/medias/linkedin.svg'} width={30} height={30} alt='linkedin'/>
+                Conectar
+              </LinkButton>
+            </li>
+          </ul>
+
+        </article>
+
+        <div></div>
       </div>
 
+      {/* Polygon */}
+      <div className='absolute right-0 top-0 w-[70%] h-full z-0
+        clip-triangle bg-c3-1'>
+      </div>
     </section>
 
   )
