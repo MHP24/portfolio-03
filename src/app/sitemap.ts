47e70fdc9cce049f5a700f7521b4e33fcdb2582c
MHP24 <mig.henriquezp@gmail.com>
2024-03-08 17:50:43 -0300

    feat: added sitemap.ts & robots.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,10 @@
+import { type MetadataRoute } from 'next'
+
+export default async function sitemap (): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_URL}/`,
+      lastModified: new Date()
+    }
+  ]
+}
